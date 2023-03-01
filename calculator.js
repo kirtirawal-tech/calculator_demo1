dc64fbf80f5fd21dc6bfc19770c69d9e35c83c6e
kirtirawal-tech <kirtirawal258@gmail.com>
2023-03-01 17:27:27 +0530

    updated in UI

--- calculator.js
+++ calculator.js
@@ -1,39 +1,30 @@
 var element = document.getElementById('digit');
 var digit1 = document.getElementById('digit_1')
-function getValue(a)
-{
-    if(element.value == 0)
-    {
-         element.value=a;
-    }   
-    else
-    {
-        element.value += a;     
+function getValue(a) {
+    if (element.value == 0) {
+        element.value = a;
+    }
+    else {
+        element.value += a;
     }
 }
 
-
- function removeEnd()
-{
-   temp = element.value
-   if(temp.length > 1)
-   {
-   element.value = temp.slice(0, -1);
-   }
-   else
-   {
-    element.value= '0';
-   }
-}   
-
-function clearAll()
-{
-    element.value= '0';
-    digit1.value= '';
+function removeEnd() {
+    temp = element.value
+    if (temp.length > 1) {
+        element.value = temp.slice(0, -1);
+    }
+    else {
+        element.value = '0';
+    }
 }
- 
-function solution()
-{
+
+function clearAll() {
+    element.value = '0';
+    digit1.value = '';
+}
+
+function solution() {
     temp = element.value;
     element.value = eval(temp);
     digit1.value = temp + " =";
